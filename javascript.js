d51ef9dcf78c46c1657800e9cc1d8e42c6cfda5d
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-05-25 10:43:20 -0300

    Responsive sheet size
    Through squaresPerSide variable

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
-const mainDiv = document.querySelector("#mainDiv");
 let squaresPerSide = 64;
+let squareSide = (100 / squaresPerSide) * .91;
+let squareSideWidthString = squareSide.toFixed(2);
+let squareSideWidthNumber = parseFloat(squareSideWidthString);
+console.log(squareSideWidthNumber);
+
+// function nuevaHoja() {
+    
+// };
+
+const boton = document.querySelector("button");
+// boton.addEventListener("click", nuevaHoja);
+const mainDiv = document.querySelector("#mainDiv");
 
 
 for (let i = 1; i < squaresPerSide + 1; i++) {
@@ -10,9 +21,11 @@ for (let i = 1; i < squaresPerSide + 1; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add("gridSquare");
         gridSquare.setAttribute("style", 
-            "background-color:blueviolet; width: 1.42vmin; height: 1.42vmin;");
+            `background-color:blueviolet; 
+            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
         gridSquare.addEventListener("mouseover", () => {gridSquare.setAttribute("style", 
-            "background-color:chocolate; width: 1.42vmin; height: 1.42vmin;")});
+            `background-color:chocolate; 
+            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}2vmin;`)});
         // gridSquare.textContent = "0";
         horizontalDiv.appendChild(gridSquare);
     }
