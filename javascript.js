5e1f0f0388aed41037c1e4de5475b28e265df44d
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-05-30 10:02:10 -0300

    Not erase everything on pencil choice

--- javascript.js
+++ javascript.js
@@ -19,60 +19,63 @@ function nuevaHoja(squaresPerSide) {
         horizontalDiv.setAttribute("style", "display: flex; flex-direction: column;");
         
             for (let i = 1; i < squaresPerSide + 1; i++) {
-            let gridSquare = document.createElement("div");
-            gridSquare.setAttribute("style", 
-                `background-color: ${colorFondo}; 
-                width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
-            gridSquare.addEventListener("mousedown", (event) => {
-                event.preventDefault();
-                if(event.which == 1) {
-                    let rojo, verde, azul;
-                    if(ArcoirisPrendido) {
-                        rojo = Math.floor(Math.random() * 255);
-                        verde = Math.floor(Math.random() * 255);
-                        azul = Math.floor(Math.random() * 255);
-                        gridSquare.setAttribute("style", 
-                            `background-color: rgb(${rojo}, ${verde}, ${azul}); 
-                            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
-                    } else if(lapizOscurecedorPrendido) {
-                        let gridBackColor = event.target.style.backgroundColor;
-                        gridBackColor = gridBackColor.split(",");
-                        let rojo = parseInt(gridBackColor[0].slice(4)) - 25;
-                        let verde = parseInt(gridBackColor[1]) - 25;
-                        let azul = parseInt(gridBackColor[2]) - 25;
-                        event.target.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
-                    } else {
-                        gridSquare.setAttribute("style", 
-                            `background-color: ${colorLapiz}; 
-                            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
-                        }
-                }    
-            });
-            gridSquare.addEventListener("mouseenter", (event) => {
-                if(event.which == 1) {
-                    let rojo, verde, azul;
-                    if(ArcoirisPrendido) {
-                        rojo = Math.floor(Math.random() * 255);
-                        verde = Math.floor(Math.random() * 255);
-                        azul = Math.floor(Math.random() * 255);
-                        gridSquare.setAttribute("style", 
-                            `background-color: rgb(${rojo}, ${verde}, ${azul}); 
-                            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
-                    } else if(lapizOscurecedorPrendido) {
-                        let gridBackColor = event.target.style.backgroundColor;
-                        gridBackColor = gridBackColor.split(",");
-                        let rojo = parseInt(gridBackColor[0].slice(4)) - 25;
-                        let verde = parseInt(gridBackColor[1]) - 25;
-                        let azul = parseInt(gridBackColor[2]) - 25;
-                        event.target.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
-                    } else {
-                        gridSquare.setAttribute("style", 
-                            `background-color: ${colorLapiz}; 
-                            width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
-                        }
-                }    
-            });
-            horizontalDiv.appendChild(gridSquare);
+                let gridSquare = document.createElement("div");
+            
+                gridSquare.setAttribute("style", 
+                    `background-color: ${colorFondo}; 
+                    width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
+            
+                gridSquare.addEventListener("mousedown", (event) => {
+                    event.preventDefault();
+                    if(event.which == 1) {
+                        let rojo, verde, azul;
+                        if(ArcoirisPrendido) {
+                            rojo = Math.floor(Math.random() * 255);
+                            verde = Math.floor(Math.random() * 255);
+                            azul = Math.floor(Math.random() * 255);
+                            gridSquare.setAttribute("style", 
+                                `background-color: rgb(${rojo}, ${verde}, ${azul}); 
+                                width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
+                        } else if(lapizOscurecedorPrendido) {
+                            let gridBackColor = event.target.style.backgroundColor;
+                            gridBackColor = gridBackColor.split(",");
+                            let rojo = parseInt(gridBackColor[0].slice(4)) - 25;
+                            let verde = parseInt(gridBackColor[1]) - 25;
+                            let azul = parseInt(gridBackColor[2]) - 25;
+                            event.target.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+                        } else {
+                            gridSquare.setAttribute("style", 
+                                `background-color: ${colorLapiz}; 
+                                width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
+                            }
+                    }    
+                });
+                
+                gridSquare.addEventListener("mouseenter", (event) => {
+                    if(event.which == 1) {
+                        let rojo, verde, azul;
+                        if(ArcoirisPrendido) {
+                            rojo = Math.floor(Math.random() * 255);
+                            verde = Math.floor(Math.random() * 255);
+                            azul = Math.floor(Math.random() * 255);
+                            gridSquare.setAttribute("style", 
+                                `background-color: rgb(${rojo}, ${verde}, ${azul}); 
+                                width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
+                        } else if(lapizOscurecedorPrendido) {
+                            let gridBackColor = event.target.style.backgroundColor;
+                            gridBackColor = gridBackColor.split(",");
+                            let rojo = parseInt(gridBackColor[0].slice(4)) - 25;
+                            let verde = parseInt(gridBackColor[1]) - 25;
+                            let azul = parseInt(gridBackColor[2]) - 25;
+                            event.target.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+                        } else {
+                            gridSquare.setAttribute("style", 
+                                `background-color: ${colorLapiz}; 
+                                width: ${squareSideWidthNumber}vmin; height: ${squareSideWidthNumber}vmin;`);
+                            }
+                    }    
+                });
+                horizontalDiv.appendChild(gridSquare);
         }
         mainDiv.appendChild(horizontalDiv);
     };
@@ -91,30 +94,30 @@ function Arcoiris() {
     botonArcoiris.classList.toggle("prendido");
     botonOscurecedor.classList.remove("prendido");
     lapizOscurecedorPrendido = false;
-    let cuerpa = document.querySelector("body");
-    cuerpa.removeChild(mainDiv);
+    // let cuerpa = document.querySelector("body");
+    // cuerpa.removeChild(mainDiv);
     ArcoirisPrendido = !ArcoirisPrendido;
-    nuevaHoja(squaresPerSide);
+    // nuevaHoja(squaresPerSide);
 }
 
 function lapizOscurecedor() {
     botonOscurecedor.classList.toggle("prendido");
     botonArcoiris.classList.remove("prendido");
     ArcoirisPrendido = false;
-    let cuerpa = document.querySelector("body");
-    cuerpa.removeChild(mainDiv);
+    // let cuerpa = document.querySelector("body");
+    // cuerpa.removeChild(mainDiv);
     lapizOscurecedorPrendido = !lapizOscurecedorPrendido;
     colorFondoR = parseInt(colorFondo.slice(1,3), 16);
     colorFondoG = parseInt(colorFondo.slice(3,5), 16);
     colorFondoB = parseInt(colorFondo.slice(5), 16);
-    nuevaHoja(squaresPerSide);
+    // nuevaHoja(squaresPerSide);
 }
 
 function nuevoColorLapiz() {
     colorLapiz = event.target.value;
-    let cuerpa = document.querySelector("body");
-    cuerpa.removeChild(mainDiv);
-    nuevaHoja(squaresPerSide);
+    // let cuerpa = document.querySelector("body");
+    // cuerpa.removeChild(mainDiv);
+    // nuevaHoja(squaresPerSide);
 }
 
 function nuevoColorFondo() {
